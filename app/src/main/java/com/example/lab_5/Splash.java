2d8c6b803af75887646bf2e28d50f638253ef08a
KushalD1999 <kushaldalal49@gmail.com>
2020-10-12 11:31:47 -0400

    Add music

--- app/src/main/java/com/example/lab_5/Splash.java
+++ app/src/main/java/com/example/lab_5/Splash.java
@@ -3,6 +3,7 @@ package com.example.lab_5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import com.example.lab_5.dummy.DummyContent;
@@ -11,20 +12,26 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class Splash extends AppCompatActivity {
+    MediaPlayer music;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        music=new MediaPlayer();
+        music=MediaPlayer.create(this,R.raw.drums);
+        music.start();
         TimerTask task=new TimerTask() {
             @Override
             public void run() {
+                music.pause();
                 finish();
+
                 startActivity(new Intent(Splash.this, ItemListActivity.class));
 
             }
         };
         Timer opening= new Timer();
-        opening.schedule(task,5000);
+        opening.schedule(task,7000);
     }
 }
